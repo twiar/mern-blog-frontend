fa80677c480a05f8a59a72bffb4cb7d138d31367
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-07-28 05:42:27 +0300

    added popular tab for posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
 
 import { Post } from "../components/Post";
@@ -9,7 +7,9 @@ import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
 import { fetchPosts, fetchTags } from "../redux/slices/posts";
 import { fetchComments } from "../redux/slices/comments";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams, useLocation } from "react-router-dom";
+
+import "../index.scss";
 
 export const Home = () => {
 	const dispatch = useDispatch();
@@ -17,6 +17,7 @@ export const Home = () => {
 	const { posts, tags } = useSelector((state) => state.posts);
 	const { comments } = useSelector((state) => state.comments);
 	const { tag } = useParams();
+	const location = useLocation();
 	const isPostsLoading = posts.status === "loading";
 	const isTagsLoading = tags.status === "loading";
 
@@ -30,6 +31,16 @@ export const Home = () => {
 		return count;
 	};
 
+	const compare = (a, b) => {
+		if (a.viewsCount + commentCount(a._id) < b.viewsCount + commentCount(b._id)) {
+			return 1;
+		}
+		if (a.viewsCount + commentCount(a._id) > b.viewsCount + commentCount(b._id)) {
+			return -1;
+		}
+		return 0;
+	};
+
 	React.useEffect(() => {
 		dispatch(fetchComments());
 		dispatch(fetchPosts());
@@ -38,35 +49,42 @@ export const Home = () => {
 
 	return (
 		<>
-			<Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
-				<Tab label="Новые" />
-				<Tab label="Популярные" />
-			</Tabs>
+			<NavLink to="/" className="navLink">
+				Новые
+			</NavLink>
+			<NavLink to="/popular" className="navLink">
+				Популярные
+			</NavLink>
 			<Grid container spacing={4}>
 				<Grid xs={8} item>
-					{(isPostsLoading ? [...Array(5)] : posts.items)
-						.filter((obj) => (tag ? obj?.tags.includes(tag) : obj))
-						.map((obj, index) =>
-							isPostsLoading ? (
-								<Post key={index} isLoading={true} />
-							) : (
-								<Post
-									key={obj._id}
-									id={obj._id}
-									title={obj.title}
-									imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ""}
-									user={obj.user}
-									createdAt={obj.createdAt
-										.substring(0, obj.createdAt.lastIndexOf(".") - 3)
-										.replace(/-/g, "/")
-										.replace(/T/g, " ")}
-									viewsCount={obj.viewsCount}
-									commentsCount={commentCount(obj._id)}
-									tags={obj.tags}
-									isEditable={userData?._id === obj.user._id}
-								/>
-							),
-						)}
+					{(isPostsLoading
+						? [...Array(5)]
+						: location.pathname == "/popular"
+						? posts.items.slice().sort(compare)
+						: tag
+						? posts.items.filter((obj) => obj?.tags.includes(tag))
+						: posts.items
+					).map((obj, index) =>
+						isPostsLoading ? (
+							<Post key={index} isLoading={true} />
+						) : (
+							<Post
+								key={obj._id}
+								id={obj._id}
+								title={obj.title}
+								imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ""}
+								user={obj.user}
+								createdAt={obj.createdAt
+									.substring(0, obj.createdAt.lastIndexOf(".") - 3)
+									.replace(/-/g, "/")
+									.replace(/T/g, " ")}
+								viewsCount={obj.viewsCount}
+								commentsCount={commentCount(obj._id)}
+								tags={obj.tags}
+								isEditable={userData?._id === obj.user._id}
+							/>
+						),
+					)}
 				</Grid>
 				<Grid xs={4} item>
 					<TagsBlock items={tags.items} isLoading={isTagsLoading} />
